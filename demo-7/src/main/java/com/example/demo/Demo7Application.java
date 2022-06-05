0595f6fb9d599ddaba580ecc3aa68b1deec0463f
Jun-Gyu Lee <yjpress@hanmail.net>
2022-06-05 10:08:22 +0900

    cx

--- demo-7/src/main/java/com/example/demo/Demo7Application.java
+++ demo-7/src/main/java/com/example/demo/Demo7Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo7Application.class, args);
+		SpringApplication.run(Demo7Application.class, args);///demo7
 	}
 
 }
